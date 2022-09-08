4989ac53ccef778f3e0364f1e7f2af59e54920fc
HugCls <superhugh92@gmail.com>
2022-09-08 19:04:28 +0200

    fetching datas on progress

--- Components/Discography.jsx
+++ Components/Discography.jsx
@@ -7,10 +7,34 @@ import {
   AccordionPanel,
   AccordionIcon,
 } from "@chakra-ui/react";
+import axios from "axios";
 import { userinfo } from "../Constants/userinfo";
 import Link from "next/link";
 
-const Discography = ({ currentTheme }) => {
+// const options = {
+//   method: "GET",
+//   url: "https://api.discogs.com/artists/1368289/releases",
+//   params: { sort: "year", sort_order: "desc", per_page: "75" },
+// };
+
+// const [releases, setReleases] = useState([]);
+
+// const getReleases = async () => {
+//   axios
+//     .request(options)
+//     .then((response) => {
+//       console.log(response.data);
+//       const allReleases = response.data;
+//       setReleases(allReleases);
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//     });
+// };
+
+useEffect(() => getReleases(), []);
+
+function Discography({ currentTheme }) {
   return (
     <Accordion allowToggle>
       <AccordionItem>
@@ -22,31 +46,31 @@ const Discography = ({ currentTheme }) => {
             <ul>
               {userinfo.discography.discographyList
                 ? userinfo.discography.discographyList.map((value, key) => {
-                    return (
-                      <li data-aos="fade-up" key={key}>
-                        <div className={styles.content}>
-                          <h3 style={{ color: currentTheme.accent }}>
-                            {value.title}
-                          </h3>
-                          <p style={{ color: currentTheme.subtext }}>
-                            {value.label}, {value.place}
-                          </p>
-                          <p style={{ color: currentTheme.subtext }}>
-                            {value.supports}
-                          </p>
-                        </div>
-                        <div
-                          className={styles.time}
-                          style={{
-                            border: `2px solid ${currentTheme.accent}`,
-                            color: currentTheme.accent,
-                          }}
-                        >
-                          <h4>{value.time}</h4>
-                        </div>
-                      </li>
-                    );
-                  })
+                  return (
+                    <li data-aos="fade-up" key={key}>
+                      <div className={styles.content}>
+                        <h3 style={{ color: currentTheme.accent }}>
+                          {value.title}
+                        </h3>
+                        <p style={{ color: currentTheme.subtext }}>
+                          {value.label}, {value.place}
+                        </p>
+                        <p style={{ color: currentTheme.subtext }}>
+                          {value.supports}
+                        </p>
+                      </div>
+                      <div
+                        className={styles.time}
+                        style={{
+                          border: `2px solid ${currentTheme.accent}`,
+                          color: currentTheme.accent,
+                        }}
+                      >
+                        <h4>{value.time}</h4>
+                      </div>
+                    </li>
+                  );
+                })
                 : null}
               <div style={{ clear: "both" }} />
             </ul>
@@ -55,6 +79,6 @@ const Discography = ({ currentTheme }) => {
       </AccordionItem>
     </Accordion>
   );
-};
+}
 
 export default Discography;
--- pages/api/discography.js
+++ pages/api/discography.js
@@ -1,13 +1,24 @@
 import axios from "axios";
 
 const options = {
-  method: 'GET',
-  url: 'https://api.discogs.com/artists/1368289/releases',
-  params: {sort: 'year', sort_order: 'desc', per_page: '75'}
+  method: "GET",
+  url: "https://api.discogs.com/artists/1368289/releases",
+  params: { sort: "year", sort_order: "desc", per_page: "75" },
 };
 
-axios.request(options).then((response) => {
-  console.log(response.data);
-}).catch((error) => {
-  console.error(error);
-});
+const [releases, setReleases] = useState([]);
+
+const getReleases = async () => {
+  axios
+    .request(options)
+    .then((response) => {
+      console.log(response.data);
+      const allReleases = response.data;
+      setReleases(allReleases);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+useEffect(() => getReleases(), []);
