b1619324ee6e95d4a28f37d964721a39b377d2bd
HugCls <superhugh92@gmail.com>
2022-10-07 20:43:47 +0200

    timeline improvement

--- Components/Discography.jsx
+++ Components/Discography.jsx
@@ -14,58 +14,55 @@ import useAxios from "../Hooks/useAxios";
 import Link from "next/link";
 
 const Discography = ({ currentTheme }) => {
-  // const options = {
-  //   method: 'GET',
-  //   url: 'https://api.discogs.com/artists/1368289/releases',
-  //   params: {sort: 'year', sort_order: 'desc', per_page: '75'},
-  //   headers: {
-  //     Authorization: 'OAuth oauth_consumer_key="", oauth_nonce="1ADA5sLK9Ia5RPJxOZZKGCSSsG0XykmI", oauth_signature="k4kYQvMiQ8zP%2BucmQJ7l9uVzLIA%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1663625431", oauth_token="goccowdGLBPzAZlfvRejYDekqKykERVdoNHCEsOA", oauth_version="1.0"',
-  //     'User-Agent': 'axios/0.19.2 X-SiteSpeedApp-1 EzoicStaticCache'
-  //   }
-  // };
+ 
 
   const { data, error, loading } = useFetchData();
+  data?.releases.splice(63);
+  data?.releases.splice(0, 1);
+  delete data?.releases[0];
 
-//  if (loading) console.log(data?.releases);
-return (
-  <Accordion allowToggle>
-    <AccordionItem>
-      <AccordionButton justifyContent="center">
-        <AccordionIcon />
-      </AccordionButton>
-      <AccordionPanel>
-        <div className={styles.timeline}>
-          <ul>
-            {data.releases.map((value, key) => {
-              return (
-                <li data-aos="fade-up" key={key}>
-                        <div className={styles.content}>
-                          <h3 style={{ color: currentTheme.accent }}>
-                            {value.artist}
-                          </h3>
-                          <h2 style={{ color: currentTheme.subtext }}>
-                            {value.title}
-                            </h2>
-                          <p style={{ color: currentTheme.subtext }}>
-                            {value.label}, Role: {value.role}
-                          </p>
-                          <p style={{ color: currentTheme.subtext }}>
-                            {value.format}
-                          </p>
-                        </div>
-                        <div
-                          className={styles.support}
-                          style={{
-                            border: `2px solid ${currentTheme.accent}`,
-                            color: currentTheme.accent,
-                          }}
-                        >
-                          <h4>{value.year}</h4>
-                        </div>
-                      </li>
-                    );
-                  })
-}
+  if (loading) console.log(data.releases);
+  return (
+    <Accordion allowToggle>
+      <AccordionItem>
+        <AccordionButton justifyContent="center">
+          <AccordionIcon />
+        </AccordionButton>
+        <AccordionPanel>
+          <div className={styles.timeline2}>
+            <ul>
+              {data?.releases.map((value, key) => {
+                return (
+                  <li data-aos="fade-up" key={key}>
+                    <div className={styles.content}>
+                      <h3 style={{ color: currentTheme.accent }}>
+                        {value.artist}
+                      </h3>
+                      <h2 style={{ color: currentTheme.subtext }}>
+                        {value.title}
+                      </h2>
+                      <p style={{ color: currentTheme.subtext }}>
+                        {value.label}
+                      </p>
+                      <p style={{ color: currentTheme.subtext }}>
+                        {value.role}
+                      </p>
+                      <p style={{ color: currentTheme.subtext }}>
+                        {value.format}
+                      </p>
+                    </div>
+                    <div
+                      className={styles.time}
+                      style={{
+                        border: `2px solid ${currentTheme.accent}`,
+                        color: currentTheme.accent,
+                      }}
+                    >
+                      <h4>{value.year}</h4>
+                    </div>
+                  </li>
+                );
+              })}
               <div style={{ clear: "both" }} />
             </ul>
           </div>
