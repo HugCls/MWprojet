9a05b6a54d5c3bdbc07df021f20d6be0d1fb2079
HugCls <superhugh92@gmail.com>
2023-04-10 22:51:10 +0200

    major refacto

--- Components/DiscoDataDisplay.jsx
+++ Components/DiscoDataDisplay.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import DiscBand from './DiscBand';
 import DiscYear from './DiscYear';
 import DiscLabel from './DiscLabel';
+import styles from '../styles/Home.module.css';
 
 
 const DiscoDataDisplay = ({ sortOption, sortedData, bandKeys, groupedData, currentTheme }) => {
@@ -9,9 +10,11 @@ const DiscoDataDisplay = ({ sortOption, sortedData, bandKeys, groupedData, curre
     if (sortOption === 'band') {
         return  bandKeys.map((band, index) => (
           <div key={index}>
+            <div data-aos="fade-up" className={styles.timeline2} >
             <h3 style={{ border: `2px solid ${currentTheme.accent}`, color: currentTheme.accent }}>
               {band}
             </h3>
+            </div>
             {groupedData[band].map((disc, index) => (
               <DiscBand disc={disc} index={index} currentTheme={currentTheme} />
             ))}
