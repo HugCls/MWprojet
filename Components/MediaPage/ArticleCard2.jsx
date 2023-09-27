8d9b4c7700372f349429f784f3c184e193762d81
HugCls <superhugh92@gmail.com>
2023-09-27 16:11:35 +0200

    component ready, refacto improvement to come.

--- Components/MediaPage/ArticleCard2.jsx
+++ Components/MediaPage/ArticleCard2.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   Box,
   TagLabel,
@@ -13,32 +14,24 @@ import {
   ModalFooter,
   useDisclosure,
 } from "@chakra-ui/react";
-import useFetchData from "../../Hooks/useFetchData";
 import Image from "next/image";
-import React from "react";
 import Parser from "html-react-parser";
 
 const ArticleCard2 = ({ articles, currentTheme }) => {
   const btnRef = React.useRef(null);
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  // const article = articles??.data[0];
-
-  console.log("articles : ", articles);
-
   const renderTags = (tags) => {
     return tags.slice(0, 3).map((category, key) => (
-      <div key={key}>
-        <Tag size="sm" borderRadius="md" variant="outline" colorScheme="blue">
-          <TagLabel>{category}</TagLabel>
-        </Tag>
-      </div>
+      <Tag key={key} size="sm" borderRadius="md" variant="outline" colorScheme="blue">
+        <TagLabel>{category}</TagLabel>
+      </Tag>
     ));
   };
 
   return (
     <Box
-      as={"div"}
+      as="div"
       style={{ whiteSpace: "initial" }}
       width="20rem"
       height={{ base: "auto", md: "47rem" }}
@@ -53,43 +46,35 @@ const ArticleCard2 = ({ articles, currentTheme }) => {
         alt="thumbnail image"
         width={articles?.cover_image.width}
         height={articles?.cover_image.height}
-      />{" "}
+      />
       <Box p="6">
         <HStack spacing={2}>
-          {articles?.tags ? renderTags(JSON.parse(articles?.tags)) : null}
+          {articles?.tags && renderTags(JSON.parse(articles.tags))}
         </HStack>
         <Box mt="1" as="h4" lineHeight="tight">
           {articles?.title}
         </Box>
         <Box as="span" color={currentTheme.subtext} fontSize="sm">
-          {articles?.description.replace(/(<([^>]+)>)/gi, "").slice(0, 80) +
-            " . . ."}
+          {`${articles?.description.replace(/(<([^>]+)>)/gi, "").slice(0, 80)} . . .`}
         </Box>
         <Button variant="link" ref={btnRef} onClick={onOpen}>
-          <a style={{ fontSize: "16px", color: "#3182ce" }}>View More</a>
+          View More
         </Button>
         <Modal onClose={onClose} finalFocusRef={btnRef} isOpen={isOpen}>
           <ModalOverlay />
-          <ModalContent style={{ backgroundColor: currentTheme.secondary }}>
+          <ModalContent bg={currentTheme.secondary}>
             <ModalHeader>{articles?.title}</ModalHeader>
             <ModalCloseButton />
             <ModalBody>
-              {articles && Parser(articles?.description)}{" "}
+              {articles && Parser(articles.description)}
               <Button variant="link">
-                <a
-                  href={articles?.link}
-                  target="_blank"
-                  style={{ fontSize: "16px", color: "#3182ce" }}
-                >
+                <a href={articles?.link} target="_blank" style={{ fontSize: "16px", color: "#3182ce" }}>
                   {articles?.phraseLink}
                 </a>
               </Button>
             </ModalBody>
             <ModalFooter>
-              <Button
-                onClick={onClose}
-                backgroundColor={currentTheme.secondary}
-              >
+              <Button onClick={onClose} bg={currentTheme.secondary}>
                 Close
               </Button>
             </ModalFooter>
--- Components/MediaPage/Media.jsx
+++ Components/MediaPage/Media.jsx
@@ -1,94 +1,52 @@
 import styles from "./Media.module.css";
-
 import MediaProject from "./MediaProject";
-import useFetchData from "../../Hooks/useFetchData";
-import ArticleCard from "./ArticleCard";
 import ArticleCard2 from "./ArticleCard2";
+import useFetchManyData from "../../Hooks/useFetchManyData";
 import BookCard from "./BookCard";
 import { custom_book } from "../../Constants/book";
-import { custom_articles } from "../../Constants/articles";
-import { useState, useEffect } from "react";
 import { headings, userinfo } from "../../Constants/userinfo";
 import Carousel, { CarouselItem } from "./Carousel";
+import { useState, useEffect } from "react";
 
 const Media = ({ currentTheme }) => {
   const [videos, setVideos] = useState(null);
   const [articles, setArticles] = useState([]);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [articleList, setArticleList] = useState([]);
-  const [bookList, setBookList] = useState([]);
+  const [bookList] = useState(custom_book);
+
+  const { data, error, loading } = useFetchManyData([
+    process.env.NEXT_PUBLIC_API_VIDEOS_URL,
+    process.env.NEXT_PUBLIC_API_PUBLICATIONS_URL
+  ]);
 
   useEffect(() => {
-    // Utilisation de Promise.all pour effectuer plusieurs appels API simultanément
-    Promise.all([
-      fetch(process.env.NEXT_PUBLIC_API_VIDEOS_URL),
-      fetch(process.env.NEXT_PUBLIC_API_PUBLICATIONS_URL),
-    ])
-      .then(async ([videosRes, articlesRes]) => {
-        const videosData = await videosRes.json();
-        const articlesData = await articlesRes.json();
-
-
-
-        setVideos(videosData);
-        setArticles(articlesData);
-        setLoading(false);
-      })
-      .catch((err) => {
-        setError(err);
-        setLoading(false);
-      });
-  }, []);
-
-  useEffect(() => {
-    setArticleList(custom_articles);
-  }, []);
-  useEffect(() => {
-    setBookList(custom_book);
-  }, []);
-
-  if (loading) return <p>Chargement...</p>;
-  if (error) return <p>Erreur lors du chargement des données.</p>;
+    if (data) {
+      setVideos(data[0]);
+      setArticles(data[1]);
+    }
+  }, [data]);
 
   return (
     <div>
       <div className={styles.workHeading}>{headings.workMainPage}</div>
       <div className={styles.workmain} style={{ color: currentTheme.subtext }}>
-        <div data-aos="fade-up">
-          <MediaProject currentTheme={currentTheme} data={videos} />
-        </div>
+        <MediaProject currentTheme={currentTheme} data={videos} />
       </div>
 
-      {userinfo.articles.visible ? (
+      <Carousel currentTheme={currentTheme}>
+        {articles.slice(0, 6).map((article) => (
+          <CarouselItem key={article.id} currentTheme={currentTheme}>
+            <ArticleCard2 articles={article.acf} currentTheme={currentTheme} />
+          </CarouselItem>
+        ))}
+      </Carousel>
+
+      {userinfo.book.visible && (
         <div style={{ backgroundColor: currentTheme.secondary }}>
-          {articleList ? (
-            <Carousel currentTheme={currentTheme}>
-              {articleList.slice(0, 6).map((article, key) => {
-                return (
-                  <CarouselItem key={key} currentTheme={currentTheme}>
-                    <ArticleCard
-                      article={article}
-                      currentTheme={currentTheme}
-                    />
-                  </CarouselItem>
-                );
-              })}
-            </Carousel>
-          ) : null}
+          {bookList.map((book) => (
+            <BookCard key={book.id} book={book} currentTheme={currentTheme} />
+          ))}
         </div>
-      ) : null}
-      {articles.map(article =>
-        <ArticleCard2 articles={article.acf} currentTheme={currentTheme} />)}
-      {userinfo.book.visible ? (
-        <div style={{ backgroundColor: currentTheme.secondary }}>
-          {bookList
-            ? bookList.map((book, key) => {
-              return <BookCard book={book} currentTheme={currentTheme} />;
-            })
-            : null}
-        </div>
-      ) : null}
+      )}
     </div>
   );
 };
